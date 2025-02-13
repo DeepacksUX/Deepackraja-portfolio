25421181710d7c9d1cfb04ba7202836c19b8e044
DeepacksUX <deepacksux@gmail.com>
2025-02-13 11:57:44 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,18 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#121212', // Dark background
+        foreground: '#ffffff', // White text
+        accent: '#9b5de5', // Purple accent
+      },
+      fontFamily: {
+        sans: ['Source Sans Pro', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
+
 export default config
